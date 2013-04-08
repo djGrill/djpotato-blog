77d982d40360800a465b8536e504052c34725613
David Grilli <djgrill@gmail.com>
2013-04-08 04:26:30 -0700

    Removes sample app Core
    deleted:    core/
    modified:   settings.py
    modified:   test.sh

--- settings.py
+++ settings.py
@@ -155,7 +155,6 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core',
     'appengine_sessions',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
